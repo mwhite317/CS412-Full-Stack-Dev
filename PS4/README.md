d9958a4cfe138b579e09e35c72e0347145d08358
Mark White <markw249@gmail.com>
2020-10-28 23:54:19 -0700

    finish assignment

---
+++ PS4/README.md
@@ -0,0 +1,8 @@
+# PS4 API Example
+This assignment allows users to search for a city then be displayed the current temp in F.
+
+## PS4 env settings
+
+This assignment requires a .env file to have the following properties defined:
+- API_ENDPOINT (The http address of the openweather map forecast API)
+- API_KEY (The string key for the openweather map API)
--- PS4/routes/PS4.js
+++ PS4/routes/PS4.js
@@ -1,28 +1,32 @@
 var express = require('express');
-var fetch = require('node-fetch')
+var fetch = require('node-fetch');
+require('dotenv').config()
 var router = express.Router();
 
-function fetchWeatherInfo(cityName) {
-  // string interpolation
-  return fetch("http://api.openweathermap.org/data/2.5/weather?q=Dallas&appid=d98c29c61c6f4f4dcde9dbfc3fc7874e")
+const API_KEY = process.env.API_KEY;
+const API_ENDPOINT = process.env.API_ENDPOINT;
+function fetchWeatherJson(cityName) {
+  // returns a promise
+  return fetch(`${API_ENDPOINT}?q=${cityName}&appid=${API_KEY}`)
+    // return value in promise clause
     .then(response => response.json())
-// handle error case
-
 }
 
 
 
-/* GET ps3 page. */
-router.get('/', function (req, res, next) {
-  fetchWeatherInfo(".").then(weatherData => {
+/* render temperature page for a city or display error */
+router.post('/', function (req, res, next) {
+  const cityName = req.body.inputCity;
+  if (cityName === ""){
+    res.render('error',{error:{status:404, stack: "please enter a valid city name"}})
+  }
+  fetchWeatherJson(cityName).then(weatherData => {
     console.log(weatherData);
-    res.render('ps4', {title: 'PS4 Assignment, ', message: weatherData.main.temp});
+    res.render('ps4', {title: `The temperature in ${cityName}:`, message: `${((weatherData.main.temp*1.8)-459.67).toFixed(1)}°F`});
+  }).catch(error => {
+    res.render('error', {error: {status: 500, stack: error.stack}});
   })
-  //handle error case
 });
 
-/* POST page. */
-router.post('/', function (req, res, next) {
-  res.render('ps4', {message: req.body.message, size: req.body.message.length});
-});
+
 module.exports = router;
